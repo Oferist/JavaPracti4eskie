b19ac32ab3c7421a807e9e68938c2f982f9fb55e
Gleb <131911698+Oferist@users.noreply.github.com>
2023-12-24 16:29:37 +0300

    Create z3.java

---
+++ Практическая №2/z3.java
@@ -0,0 +1,12 @@
+package pr2;
+
+/* Вывести на экран первые 10 чисел гармонического ряда. */
+
+public class z3 {
+	public static void main(String[] args) {
+        System.out.println("Первые 10 чисел гармонического ряда:");
+        for (int i = 1; i <= 10; i++) {
+        	System.out.println(1./i);
+        }
+    }
+}
