6a5d02546340b7494429c8abcdf094935df78cdd
Gleb <131911698+Oferist@users.noreply.github.com>
2023-12-24 16:53:30 +0300

    Add files via upload

---
+++ pr8_z1/Shape.java
@@ -0,0 +1,18 @@
+package pr8_z1;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public abstract class Shape {
+    protected Color color;
+    protected int x;
+    protected int y;
+
+    public Shape(Color color, int x, int y) {
+        this.color = color;
+        this.x = x;
+        this.y = y;
+    }
+
+    abstract void draw(Graphics g);
+}
